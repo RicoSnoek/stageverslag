374261c3b722a5b22054e26d8c47d7f56a754ed7
RicoSnoek <rico-8888@hotmail.com>
2017-02-08 16:31:13 +0100

    changed controller for interval

--- public/core.controller.js
+++ public/core.controller.js
@@ -1,45 +1,61 @@
 dashboard.controller('mainController', ['$interval', '$scope', 'DutyData', 'GetFeed', 'GetSheet','GetBirthdays', 'GoogleCalendar', function($interval, $scope, DutyData, GetFeed, GetSheet,GetBirthdays, GoogleCalendar) {
 
-    GetFeed.getFeedData().then(function(res){
-        console.log(res.data);
-         $scope.feeds = res.data;
-    }).catch(function(err) {
-        console.log(err);
-    });
-    GetSheet.getSheetData().then(function(res) {
-        console.log(res);
-        $scope.sheet = res;
-    }).catch(function(err) {
-        console.log(err);
-    });
+    var getAllFeedData = function() {
+        GetFeed.getFeedData().then(function(res){
+            console.log(res.data);
+             $scope.feeds = res.data;
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+    var getAllSheetData = function() {
+        GetSheet.getSheetData().then(function(res) {
+            console.log(res);
+            $scope.sheet = res;
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+    var getAllCalendarData = function() {
+        GoogleCalendar.getCalendarData().then(function(data) {
+            console.log(data);
+            $scope.events = data.data;
+            $scope.visitors = data.visitors;
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+    var getAllDutyData = function(){
+        DutyData.getDutyData().then(function(duty) {
+            console.log(duty);
+            $scope.duty = duty;
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
 
-    GoogleCalendar.getCalendarData().then(function(data) {
-        console.log(data);
-        $scope.events = data.data;
-        $scope.visitors = data.visitors;
-    }).catch(function(err) {
-        console.log(err);
-    });
-    DutyData.getDutyData().then(function(duty) {
-        console.log(duty);
-        $scope.duty = duty;
-    }).catch(function(err) {
-        console.log(err);
-    });
+    var getAllBirthdayData = function() {
+        GetBirthdays.getBirthdayData().then(function(birthdays) {
+            if(birthdays != null) {
+                $scope.birthdays = birthdays; 
+            }
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
 
-    GetBirthdays.getBirthdayData().then(function(birthdays) {
-        if(birthdays != null) {
-            $scope.birthdays = birthdays; 
-        }
+    getAllFeedData();
+    getAllSheetData();
+    getAllCalendarData();
+    getAllDutyData();
+    getAllBirthdayData();
 
-    }).catch(function(err) {
-        console.log(err);
-    });
-
-    // updateDuty = $interval(function() {
-    //     DutyData.getDutyData();
-    //     console.log('Updated');
-    // }, 36000000);
-
+    updateData = $interval(function() {
+        getAllFeedData();
+        getAllSheetData();
+        getAllCalendarData();
+        getAllDutyData();
+        getAllBirthdayData();
+    }, 600 * 1000);
 
 }]);
